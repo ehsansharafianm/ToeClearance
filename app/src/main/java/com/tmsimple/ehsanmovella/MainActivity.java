baf33bf07d0e3143e8731b0e2a3855f9b73355e3
esharafianm <esharafianm@gmail.com>
2024-05-24 17:19:13 -0400

    Crosby second Version

--- app/src/main/java/com/tmsimple/ehsanmovella/MainActivity.java
+++ app/src/main/java/com/tmsimple/ehsanmovella/MainActivity.java
@@ -65,6 +65,7 @@ import android.widget.Switch;
 import org.tensorflow.lite.Interpreter;
 
 // This version is edited by Crosby computer
+// Second Change from Crosby
 public class MainActivity extends AppCompatActivity implements DotDeviceCallback, DotScannerCallback, DotRecordingCallback, DotSyncCallback, DotMeasurementCallback {
 
     public String Version = "v1.2";
